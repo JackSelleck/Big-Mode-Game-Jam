e1191724a6faf307eb743e265c00b75a8a1a4000
Jack Selleck <jackselleck2004@gmail.com>
2025-01-25 19:31:06 +0000

    Cleaning up typing game

--- Assets/Scripts/Computer/TypingGame.cs
+++ Assets/Scripts/Computer/TypingGame.cs
@@ -15,7 +15,7 @@ public class TypingGame : MonoBehaviour
     }
     private void DecideText()
     {
-        if (decideText == 0) { text.text = "bigmode"; }
+        if (decideText == 0) { text.text = "go bigmode"; }
         if (decideText == 1) { text.text = "spagetti"; }
         if (decideText == 2) { text.text = "meatballs"; }
         if (decideText == 3) { text.text = "blueberry muffin"; }
@@ -23,24 +23,25 @@ public class TypingGame : MonoBehaviour
         if (decideText == 5) { text.text = "not even close baby!"; }
         if (decideText == 6) { text.text = "pizza butt"; }
         if (decideText == 7) { text.text = "ooh ooh ah ah"; }
-        if (decideText == 8) { text.text = "muffin"; }
+        if (decideText == 8) { text.text = "and i better get it too"; }
         if (decideText == 9) { text.text = "donkey kong december"; }
         if (decideText == 10){ text.text = "this game really makes you FEEL overworked"; }
         if (decideText == 11){ text.text = "huge drama"; }
         if (decideText == 12){ text.text = "goblins are real"; }
         if (decideText == 13){ text.text = "i want tacos"; }
-        if (decideText == 14){ text.text = "Bigmode"; }
-        if (decideText == 15){ text.text = "Bigmode"; }
-        if (decideText == 16){ text.text = "Bigmode"; }
-        if (decideText == 17){ text.text = "Bigmode"; }
-        if (decideText == 18){ text.text = "Bigmode"; }
-        if (decideText == 19){ text.text = "Bigmode"; }
-        if (decideText == 20){ text.text = "Bigmode"; }
+        if (decideText == 14){ text.text = "mario bros 2"; }
+        if (decideText == 15){ text.text = "frozen ape"; }
+        if (decideText == 16){ text.text = "beeg yoshi"; }
+        if (decideText == 17){ text.text = "ikon clan"; }
+        if (decideText == 18){ text.text = "three beans please"; }
+        if (decideText == 19){ text.text = "hogans castle"; }
+        if (decideText == 20){ text.text = "bowsers big bean burrito"; }
     }
     private void Update()
     {
         if (playersText.text == text.text)
         {
+            gameObject.SetActive(false);
             SceneManager.LoadScene("Office");
             Debug.Log("Writing Task Complete");
         }
