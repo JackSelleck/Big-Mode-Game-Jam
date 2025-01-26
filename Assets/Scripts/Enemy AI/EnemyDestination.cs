d7c50992aa049a0a04dd16d42bc95ffa9e515f8d
Wait2Late <w82l8.islam@gmail.com>
2025-01-26 18:23:44 +0100

    A script to make a destination for the enemy

---
+++ Assets/Scripts/Enemy AI/EnemyDestination.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+namespace Enemy_AI
+{
+    public class EnemyDestination : GhostBase
+    {
+        private bool lineOfSight = false;
+        [SerializeField] private float speed = 2.0f;
+
+        private void Update()
+        {
+            if (!lineOfSight) return;
+
+            transform.position = Vector2.MoveTowards(transform.position, PlayerRef.transform.position, speed * Time.deltaTime);
+        }
+
+        private void FixedUpdate()
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, PlayerRef.transform.position - transform.position);
+            
+            if (hit.collider == null) return;
+            
+            if (hit.collider.gameObject == PlayerRef)
+            {
+                Debug.DrawRay(transform.position, PlayerRef.transform.position - transform.position, Color.red);
+                lineOfSight = true;
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, PlayerRef.transform.position - transform.position, Color.green);
+                lineOfSight = false;
+            }
+        }
+    }
+}
