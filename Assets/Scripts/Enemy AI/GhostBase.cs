6a934b1bc52a2d14ceacf7afe8d0354aa3d6f974
Wait2Late <w82l8.islam@gmail.com>
2025-01-26 18:23:30 +0100

    New base script for the enemies

---
+++ Assets/Scripts/Enemy AI/GhostBase.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Enemy_AI
+{
+    public class GhostBase : MonoBehaviour
+    {
+        public GameObject PlayerRef;
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        
+        
+    }
+}
