932389a6dc9b7cd7c3370f6bdefe03c6fa3d7543
Wait2Late <w82l8.islam@gmail.com>
2025-01-28 11:57:19 +0100

    New script for the blue ghost

---
+++ Assets/Scripts/Enemy AI/GhostBlue.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Enemy_AI
+{
+    public class GhostBlue : GhostBase
+    {
+        protected void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("TriggerEvent")) BlueGhostAttack();
+        }
+        private void BlueGhostAttack()
+        {
+            //TODO implement drain energy. Somehow.
+            
+            GhostTouch(this);
+        }
+    }
+}
