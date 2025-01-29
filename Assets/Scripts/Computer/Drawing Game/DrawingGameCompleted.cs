8a060ccaf535d8175d8a6c07bc0862f44878c758
Jack Selleck <jackselleck2004@gmail.com>
2025-01-29 22:51:00 +0000

    Making paper dissapear, making randomisation better,  adding levels
    Do not commit to main

--- Assets/Scripts/Computer/Drawing Game/DrawingGameCompleted.cs
+++ Assets/Scripts/Computer/Drawing Game/DrawingGameCompleted.cs
@@ -1,9 +1,12 @@
 using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DrawingGameCompleted : MonoBehaviour
 {
+    public GameObject taskParent;
+    public GameObject officeParent;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
@@ -15,6 +18,8 @@ public class DrawingGameCompleted : MonoBehaviour
     IEnumerator wait()
     {
         yield return new WaitForSeconds(1f);
-        SceneManager.LoadScene("Office");
+        taskParent.SetActive(true);
+        officeParent.SetActive(false);
+        //SceneManager.LoadScene("Office");
     }
 }
--- Assets/Scripts/Papers/ChooseRandomTask.cs
+++ Assets/Scripts/Papers/ChooseRandomTask.cs
@@ -1,10 +1,17 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChooseRandomTask : MonoBehaviour
 {
-    private int taskDecider;
+    public List<int> taskOptions = new List<int> { 0, 1, 2 };
+
+    public GameObject officeMap;
+    public GameObject writingTask;
+
+    public int lastChosenTask; // Store last chosen task
+    public int taskDecider;
     public TextMeshProUGUI text;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,10 +28,8 @@ public class ChooseRandomTask : MonoBehaviour
         if (collision.CompareTag("Papers") && Input.GetKey(KeyCode.E))
         {
             Debug.Log("Task Started");
-            taskDecider = Random.Range(0, 3);
             ChooseTask();
-            collision.gameObject.SetActive(false);  
-            
+            collision.gameObject.SetActive(false);        
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -37,18 +42,36 @@ public class ChooseRandomTask : MonoBehaviour
 
     private void ChooseTask()
     {
-        if (taskDecider == 0)
+        // Remove the last chosen task from the list
+        if (lastChosenTask == taskDecider)
         {
-            SceneManager.LoadScene("Typing Game");
+            taskOptions.Remove(lastChosenTask);
         }
-        if (taskDecider == 1)
+
+        // Pick a random task from the remaining options
+        taskDecider = taskOptions[Random.Range(0, taskOptions.Count)];
+
+        lastChosenTask = taskDecider; // Store this as the last task
+
+        Debug.Log($"Chosen task: {taskDecider}");
+
+        // Load the corresponding scene
+        switch (taskDecider)
         {
-            SceneManager.LoadScene("Drawing Game");
+            case 0:
+                //SceneManager.LoadScene(("Typing Game"), LoadSceneMode.Additive);
+                writingTask.SetActive(true);
+                officeMap.SetActive(false);
+                break;
+            case 1:
+                //SceneManager.LoadScene(("Drawing Game"), LoadSceneMode.Additive);
+
+                break;
+            case 2:
+                //SceneManager.LoadScene(("Maze Game"), LoadSceneMode.Additive);
+                break;
         }
-        if (taskDecider == 2)
-        {
-            SceneManager.LoadScene("Maze Game");
-        }
+
     }
 
 }
