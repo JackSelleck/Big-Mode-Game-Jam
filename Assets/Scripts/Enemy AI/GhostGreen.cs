a7e682c5c82b5c2312298207f04f7e1531ef83d6
Wait2Late <w82l8.islam@gmail.com>
2025-01-29 19:15:21 +0100

    Green ghost sends back the player to the office.

--- Assets/Scripts/Enemy AI/GhostGreen.cs
+++ Assets/Scripts/Enemy AI/GhostGreen.cs
@@ -1,9 +1,12 @@
+using Player;
 using UnityEngine;
 
 namespace Enemy_AI
 {
     public class GhostGreen : GhostBase
     {
+        [SerializeField] private Transform SendPlayerBackToOffice;
+        
         protected void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.CompareTag("TriggerEvent")) GreenGhostAttack();
@@ -11,6 +14,10 @@ namespace Enemy_AI
         private void GreenGhostAttack()
         {
             //TODO implement sending the player back to their desk. Somehow.
+            
+            BasePlayer.Instance.PlayerRespawn();
+            
+            // SendPlayerBackToOffice.position = Player.transform.position;
             
             GhostTouch(this);
         }
