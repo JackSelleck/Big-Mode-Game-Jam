16e8254679c00da395b5767950600bd449f527b5
Wait2Late <w82l8.islam@gmail.com>
2025-01-30 15:51:44 +0100

    Relocated in the BaseGhost instead

--- Assets/Scripts/Enemy AI/Navigation/EnemyDestination.cs
+++ Assets/Scripts/Enemy AI/Navigation/EnemyDestination.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -12,16 +13,6 @@ namespace Enemy_AI
         [SerializeField] private EnemyPatrolling Patrol;
         
         private bool lineOfSight = false;
-
-        private NavMeshAgent agent;
-        private void Start()
-        {
-            agent = GetComponent<NavMeshAgent>();
-            agent.updateRotation = false;
-            agent.updateUpAxis = false;
-
-            gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
-        }
         private void Update()
         {
             if (!agent.isOnNavMesh) return;
