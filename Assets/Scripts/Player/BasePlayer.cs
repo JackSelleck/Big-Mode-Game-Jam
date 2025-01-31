c3c92723105a4d73bad4a7ea3ce2adee97a95b6f
Wait2Late <w82l8.islam@gmail.com>
2025-01-31 12:30:40 +0100

    Update BasePlayer.cs

--- Assets/Scripts/Player/BasePlayer.cs
+++ Assets/Scripts/Player/BasePlayer.cs
@@ -9,6 +9,8 @@ namespace Player
     public class BasePlayer : MonoBehaviour
     {
         public static BasePlayer Instance { get; private set; }
+        
+        public bool PlayerIsDead { get; private set; }
 
         [SerializeField] private Transform RespawnPoint;
         
@@ -31,24 +33,25 @@ namespace Player
         private void OnDisable() => OnPlayerDeath -= PlayerDeath;
         private void PlayerDeath()
         {
+            PlayerIsDead = true;
             //TODO lose all progress. Somehow.
-            gameObject.SetActive(false);
-            Debug.Log("Player dead");
-
-
-            //TODO experiment with this later.
-            // sr.enabled = false;
-            // rb.bodyType = RigidbodyType2D.Static;
-            //lerp the player color from yellow to clear
+            // gameObject.SetActive(false);
+            // Debug.Log("Player dead");
             
-            // PlayerRespawn();
-            // StartCoroutine(PlayerRespawn());
-            // Invoke(nameof(PlayerRespawn), 2);
             
-            //TODO instead of respawn. Just reload the scene.
-            
-            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(currentSceneIndex);
+            // //TODO experiment with this later.
+            // // sr.enabled = false;
+            // // rb.bodyType = RigidbodyType2D.Static;
+            // //lerp the player color from yellow to clear
+            //
+            // // PlayerRespawn();
+            // // StartCoroutine(PlayerRespawn());
+            // // Invoke(nameof(PlayerRespawn), 2);
+            //
+            // //TODO instead of respawn. Just reload the scene.
+            //
+            // int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            // SceneManager.LoadScene(currentSceneIndex);
         }
         public void PlayerRespawn()
         {
