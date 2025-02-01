336f3d0cfddd8e651a23baceb0c883234a4cea50
Wait2Late <w82l8.islam@gmail.com>
2025-02-01 12:31:11 +0100

    Updated main menu canvas to include credits

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,9 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject creditsPanel;
+    
+    private bool isCreditsActive;
+    
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CreditsButton()
+    {
+        creditsPanel.SetActive(!creditsPanel.activeSelf);
+        // creditsPanel.SetActive(active);
     }
     
     public void QuitGame()
